b9f5196f1247e4698dedc4646386e09b57d6c87e
NicoleH87 <nicoleh87@nycstudents.net>
2022-03-13 21:58:08 -0400

    U8L6b

--- src/Runner.java
+++ src/Runner.java
@@ -13,16 +13,21 @@ public class Runner {
          */
 
         Encryptor deRun = new Encryptor(3, 2);
-        System.out.println(deRun.decryptMessage("Ti hsiscn ofietdnia!Bl e nto helo okotAu!A"));
+        //System.out.println(deRun.decryptMessage("Ti hsiscn ofietdnia!Bl e nto helo okotAu!A"));
 
         Encryptor deRun2 = new Encryptor(5, 5);
-        System.out.println(deRun2.decryptMessage("Tite hsocmi presa es  stsaneregtd see J  nfaBidomonee  d ftm.cuhe alisBr,ssaieecgllstelft!  -rAwsduA"));
+        //System.out.println(deRun2.decryptMessage("Tite hsocmi presa es  stsaneregtd see J  nfaBidomonee  d ftm.cuhe alisBr,ssaieecgllstelft!  -rAwsduA"));
 
         Encryptor deRun3 = new Encryptor(6, 5);
-        System.out.println(deRun3.decryptMessage("Meottai ooufdsl rtdcsrnelh e r etWwht hhiihmselgro.nlhen    sld tmc egoehsne  otttcbous oacHusAAkoloAA?ploAA eynAA f !AA"));
+        //System.out.println(deRun3.decryptMessage("Meottai ooufdsl rtdcsrnelh e r etWwht hhiihmselgro.nlhen    sld tmc egoehsne  otttcbous oacHusAAkoloAA?ploAA eynAA f !AA"));
 
         Encryptor deRun4 = new Encryptor(9, 8);
-        System.out.println(deRun4.decryptMessage("Io ihscnongeoesofn rnn  do.cac poir rprirfnmTyhyso gahpyp ce tit,tteniis  siea o,pc gpttr rotirihmkonhneoeancveasn toee le iiwsro nononstrrtffn atttencxlseoe oitl x rfwp.yptanonh ,l,nar eI a  tmardoiiai stennnlvk ealyit real  zid rcaaaeeeatkinudsc e ndt  icxtt hpcpitoeaoaahp  xcrrnehbptceo tc t  sriird pisinoyoirn gfnpetettio.tssvehnr i eereamEonlnf lannoftert eetoite tl "));
+        //System.out.println(deRun4.decryptMessage("Io ihscnongeoesofn rnn  do.cac poir rprirfnmTyhyso gahpyp ce tit,tteniis  siea o,pc gpttr rotirihmkonhneoeancveasn toee le iiwsro nononstrrtffn atttencxlseoe oitl x rfwp.yptanonh ,l,nar eI a  tmardoiiai stennnlvk ealyit real  zid rcaaaeeeatkinudsc e ndt  icxtt hpcpitoeaoaahp  xcrrnehbptceo tc t  sriird pisinoyoirn gfnpetettio.tssvehnr i eereamEonlnf lannoftert eetoite tl  unondhlctltrceeioode.en gn -r  iiitabcFbenbe eeoustlnw pr annn  patlsc uusne ,rsssedcr ycueuoheaphasdmnantel o is imla-ecoeoey rnc eyoIsotrkr rts  yeaai idtpytntibeht e hslceigdam er oe l.p ymnnbg otpests  r-dtshstb d iaoehuaeeoguset snnets ,wic   ik egrswpneflnyciogyolephesearsi dmi te lr.edcisale ,eooonsqA rmnud uncapar ai obulcsrranlt ekeeut naceehrhrtsr  eooe iymw vrcclpeikiiiaytstedzpn  shyeei dta  ddeeehgtp brribthuAyi puoosA gtit reAtioe uirAhn nnnzsAeartoae.A testudAAooc  t AA"));
 
+        Encryptor message = new Encryptor(5, 3);
+        //System.out.println(message.encryptMessage("Hope you have an eggstra good day!"));
+
+        Encryptor recieve = new Encryptor(2, 6);
+        System.out.println(recieve.decryptMessage("Wtsih  faile?i   filLesif  geaEl,nniswdhho a rtt nerc  aabtnst  letoadoth naelggrauesAAA.AAAAAAA"));
     }
 }
